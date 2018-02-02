51c87c9b2bebcecbb445c9d6f97247a7fa4f85e9
Edgardo Campos <xotl@Edgardos-MacBook-Pro.local>
2018-02-02 11:05:10 -0600

    Now parses the skills of the card

--- src/index.js
+++ src/index.js
@@ -7,15 +7,43 @@ const
 const DBS_DATA_URL = 'http://www.dbs-cardgame.com/cardlist/?search=true&category=428001'
 
 const parseSkill = rawHtml => {
-    return rawHtml
+
+    const regexp = /<img.*?alt="(.*?)".*?>/g;
+    let match, skillKeywords = [];
+    while ((match = regexp.exec(rawHtml)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (match.index === regexp.lastIndex) {
+            regexp.lastIndex++;
+        }
+
+        skillKeywords.push(match[1])
+    }
+    
+    const skillDescription = rawHtml.replace(regexp, '[$1]').replace(/<br\/?>/g, '\n')
+    return { skillDescription, skillKeywords }
+}
+
+const parseSeries = rawHtml => {
+    const series = rawHtml.split(/<br\/?>/g)
+    return {
+        seriesName: series[0],
+        seriesFullName: series[1].replace(/\&\#xFF5E; ?/g, '')
+    }
+
 }
 
 const getBackCard = elem => {
-    const find = elem.find.bind(elem)
+    const find = elem.find.bind(elem),
+          { seriesName, seriesFullName } = parseSeries( find('dl.seriesCol dd').html() ),
+          { skillDescription, skillKeywords } = parseSkill( find('dl.skillCol dd').html() )
+
     return {
+        seriesName,
+        seriesFullName,
+        skillDescription,
+        skillKeywords,
         'cardNumber': find('dt.cardNumber').text(),
         'cardName': find('dd.cardName').text(),
-        'series': find('dl.seriesCol dd').text(),
         'rarity': find('dl.rarityCol dd').text(),
         'type': find('dl.typeCol dd').text(),
         'color': find('dl.colorCol dd').text(),
@@ -23,7 +51,6 @@ const getBackCard = elem => {
         'character': find('dl.characterCol dd').text(),
         'specialTrait': find('dl.specialTraitCol dd').text(),
         'era': find('dl.eraCol dd').text(),
-        'skill': parseSkill( find('dl.skillCol dd').html() ),
         'availableDate': find('dl.availableDateCol dd').text(),
     }
 }
@@ -36,26 +63,30 @@ Fetch(DBS_DATA_URL)
             $('ul.list-inner').children().map( (index, cardDomHtml) => {
                 const elem = $(cardDomHtml),
                       cardFront = elem.find('.cardFront'),
-                      find = cardFront.find.bind(cardFront)
+                      find = cardFront.find.bind(cardFront),
+                      { seriesName, seriesFullName } = parseSeries( find('dl.seriesCol dd').html() ),
+                      { skillDescription, skillKeywords } = parseSkill( find('dl.skillCol dd').html() ),
+                      type = find('dl.typeCol dd').text()
 
-                const type = find('dl.typeCol dd').text()
                 return {
+                    type,
+                    seriesName,
+                    seriesFullName,
+                    skillDescription,
+                    skillKeywords,
                     'cardNumber': find('dt.cardNumber').text(),
                     'cardName': find('dd.cardName').text(),
-                    'series': find('dl.seriesCol dd').text(),
                     'rarity': find('dl.rarityCol dd').text(),
-                    'type': type,
                     'color': find('dl.colorCol dd').text(),
                     'power': find('dl.powerCol dd').text(),
                     'character': find('dl.characterCol dd').text(),
                     'specialTrait': find('dl.specialTraitCol dd').text(),
                     'era': find('dl.eraCol dd').text(),
-                    'skill': parseSkill( find('dl.skillCol dd').html() ),
                     'availableDate': find('dl.availableDateCol dd').text(),
                     'cardBack': type === 'LEADER' ? getBackCard( elem.find('.cardBack') ) : null
                 }
 
             } ).get()
     )
-    .then( allCards => allCards[0] )
+    .then( allCards => allCards[100] )
     .then( console.log )
