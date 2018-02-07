d1e61f5a9342e64b967332cbd09b0169d8342229
Edgardo Campos <xotl@Edgardos-MacBook-Pro.local>
2018-02-07 15:12:09 -0600

    Clean up

--- src/index.js
+++ src/index.js
@@ -99,6 +99,6 @@ const scrapMultipleUrls = urls => Promise.all( urls.map( scrapUrl ) ).then( resu
 scrapMultipleUrls(DBS_DATA_URLS)
     .then( allCards => {
         const outputPath = process.env.OUTPUT_PATH || `${__dirname}/../cards.json`
-        console.log('Wow, such total of cards', allCards.length, allCards[230])
+        console.log(`Finished with a total of ${allCards.length} cards.`)
         fs.writeFileSync( outputPath, JSON.stringify(allCards) )
     } )
