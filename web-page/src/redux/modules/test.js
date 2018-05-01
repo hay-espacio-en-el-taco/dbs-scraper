57912b81074ba8bff32854689e4bf2bec8c4e761
Edgardo Campos <xotl@Edgardos-MacBook-Pro.local>
2018-05-01 13:58:33 -0500

    Now imports only the needed rxjs operators

--- web-page/src/redux/modules/test.js
+++ web-page/src/redux/modules/test.js
@@ -1,6 +1,8 @@
 'use stric';
-import Rx from 'rxjs';
-// import 'rxjs/add/observable/of';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/ignoreElements';
 
 // Initial State
 const initState = {
