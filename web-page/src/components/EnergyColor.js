f7e56771743b23f8875c1f8a9c3a47f88cd82c66
Dio Brando <lordzero0000@gmail.com>
2019-04-29 21:30:41 -0500

    Add component to render the energy

---
+++ web-page/src/components/EnergyColor.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './EnergyColor.css';
+
+const
+    reg = /[\dbugry+]/g,
+    filterColors = e => e.match(reg),
+    iconPrinter = e => filterColors(e).map(i => {
+        if (!isNaN(Number(i))) {
+          return <div className="energy-color white">{i}</div>
+        }
+
+        switch (i) {
+          case 'b':
+              return <div className="energy-color black"></div>
+          case 'u':
+              return <div className="energy-color light-blue accent-4"></div>
+          case 'g':
+              return <div className="energy-color green accent-4"></div>
+          case 'r':
+              return <div className="energy-color red accent-4"></div>
+          case 'y':
+              return <div className="energy-color yellow accent-2"></div>
+          default:
+              return <div className="energy-color white">{i}</div>
+        }
+    })
+
+class EnergyColor extends Component {
+
+    render() {
+        const { energy } = this.props;
+        if (!energy) return <div>{energy}</div>;
+
+        return (
+            <div key={energy}>
+                {iconPrinter(energy)}
+            </div>
+        )
+    }
+}
+
+export default EnergyColor;
