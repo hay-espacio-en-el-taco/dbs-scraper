c58db1b5d9031e09b42548185b807b6ac78baebd
Dio Brando <lordzero0000@gmail.com>
2019-10-26 23:39:34 -0500

    Add use of eslint to web-page

--- web-page/src/components/Filter.js
+++ web-page/src/components/Filter.js
@@ -5,7 +5,7 @@ class FilterBox extends Component {
         const { onFilterRemove, id } = this.props;
         return (
             <li key={id}>
-                <button className="waves-effect waves-light btn" onClick={_ => onFilterRemove(id)}>
+                <button className="waves-effect waves-light btn" onClick={() => onFilterRemove(id)}>
                     <span>{id}</span> | [X]
                 </button>
             </li>
