f687b5b4a15233821911cd033df14237e460ed3c
Xotl <xotl@Surface-Xotl.localdomain>
2019-12-29 21:25:20 -0600

    Fixes path for cards.json on web app

--- tools/get-cards-file.sh
+++ tools/get-cards-file.sh
@@ -1,4 +1,4 @@
-CARDS_FILE=./web-page/cards.json
+CARDS_FILE=./web-page/src/cards.json
 if [ ! -f "$CARDS_FILE" ]; then
     docker run --name dbs-scraper dbs-scraper
     docker cp dbs-scraper:/home/node/app/cards.json $CARDS_FILE
