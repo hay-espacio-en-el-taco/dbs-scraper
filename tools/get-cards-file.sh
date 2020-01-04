bb8bac36f846b0f37c0b79feb1f544994de884ae
Xotl <xotl@Surface-Xotl.localdomain>
2020-01-04 11:46:58 -0600

    Adding an update.yml

--- tools/get-cards-file.sh
+++ tools/get-cards-file.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CARDS_FILE=./web-page/src/cards.json
+CARDS_FILE=${CARDS_OUTPUT:-./web-page/src/cards.json}
 if [ ! -f "$CARDS_FILE" ]; then
     docker run --name dbs-scraper dbs-scraper
     docker cp dbs-scraper:/home/node/app/cards.json $CARDS_FILE
