8f690ad71f4c41446cd84bc998251f5d5363cade
Xotl <xotl@Surface-Xotl.localdomain>
2020-01-12 00:41:31 -0600

    🤔

--- tools/get-cards-file.sh
+++ tools/get-cards-file.sh
@@ -4,7 +4,7 @@ CHANGELOG_FILE=${CARDS_CHANGELOG_OUTPUT:-./web-page/src/changelog.cards.txt}
 CARDS_CONTAINER="/home/node/app/cards.json"
 CHANGELOG_CONTAINER="/home/node/app/changelog.cards.txt"
 if [ ! -f "$CARDS_FILE" ]; then
-    docker run --name dbs-scraper dbs-scraper /bin/bash -c "npm start node /home/node/app/src/tools/changelog.js ${CARDS_CONTAINER}"
+    docker run --name dbs-scraper dbs-scraper /bin/bash -c "npm start && node /home/node/app/src/tools/changelog.js ${CARDS_CONTAINER}"
     docker cp dbs-scraper:${CARDS_CONTAINER} ${CARDS_FILE}
     docker cp dbs-scraper:${CHANGELOG_CONTAINER} ${CHANGELOG_FILE}
     docker rm dbs-scraper
