129c963b3efbdedd60defa68909558e7316b418d
github <noreply@github.com>
2020-02-17 04:04:54 +0000

    web_2020-02-17/04h-04m | Auto pushed from Github Actions

---
+++ precache-manifest.e746f2221a1ce64c5d3d7b1535c32241.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5c010a37c62d9d1e3a1f7f493629d6",
+    "url": "/dbs-scraper/index.html"
+  },
+  {
+    "revision": "134022782a670dc14f27",
+    "url": "/dbs-scraper/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "f8571c5e70f56c2e4d02",
+    "url": "/dbs-scraper/static/css/main.9ffbab19.chunk.css"
+  },
+  {
+    "revision": "134022782a670dc14f27",
+    "url": "/dbs-scraper/static/js/2.94691d02.chunk.js"
+  },
+  {
+    "revision": "cb6501f70cc38f376138f1a2235071f1",
+    "url": "/dbs-scraper/static/js/2.94691d02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8571c5e70f56c2e4d02",
+    "url": "/dbs-scraper/static/js/main.64854aec.chunk.js"
+  },
+  {
+    "revision": "2a995a084876425127b3",
+    "url": "/dbs-scraper/static/js/runtime-main.28c0cd4a.js"
+  }
+]);
