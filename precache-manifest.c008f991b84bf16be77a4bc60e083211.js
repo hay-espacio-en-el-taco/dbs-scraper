dd090a6e5748e1785ecc05a1e974b290c7fc8813
github <noreply@github.com>
2020-04-22 03:53:10 +0000

    web_2020-04-22/03h-53m | Auto pushed from Github Actions

---
+++ precache-manifest.c008f991b84bf16be77a4bc60e083211.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b7452044c548c0e6a60c74b2bf8533",
+    "url": "/dbs-scraper/index.html"
+  },
+  {
+    "revision": "cd47f8614af43e2b37ed",
+    "url": "/dbs-scraper/static/css/2.658eb2da.chunk.css"
+  },
+  {
+    "revision": "f77575a3fb7e1fe8ee16",
+    "url": "/dbs-scraper/static/css/main.53c7e001.chunk.css"
+  },
+  {
+    "revision": "cd47f8614af43e2b37ed",
+    "url": "/dbs-scraper/static/js/2.baa236bb.chunk.js"
+  },
+  {
+    "revision": "af25e34b1e557720666e3069c4d9449e",
+    "url": "/dbs-scraper/static/js/2.baa236bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f77575a3fb7e1fe8ee16",
+    "url": "/dbs-scraper/static/js/main.52f250e4.chunk.js"
+  },
+  {
+    "revision": "a736507908bbda81a0da",
+    "url": "/dbs-scraper/static/js/runtime-main.d6f44d3b.js"
+  }
+]);
--- tools/get-cards-file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CARDS_OUTPUT=${CARDS_OUTPUT:-./web-page/src/cards.json}
-CARDS_CONTAINER="/home/node/app/cards.json"
-if [ ! -f "$CARDS_OUTPUT" ]; then
-    docker run --name dbs-scraper dbs-scraper npm start
-    docker cp dbs-scraper:${CARDS_CONTAINER} ${CARDS_OUTPUT}
-    docker rm dbs-scraper
-    echo "Cards file extracted to ${CARDS_OUTPUT}"
-fi
