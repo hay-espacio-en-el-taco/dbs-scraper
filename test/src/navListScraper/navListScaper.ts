ceebcdc75ba52dba9a3d1282fb48c56a0c1003b0
Kyle Frick <102466@nelnet.net>
2022-05-11 17:31:41 -0400

    chore: refactor

--- test/src/navListScraper/navListScaper.ts
+++ test/src/navListScraper/navListScaper.ts
@@ -1,18 +1,26 @@
 import axios from "axios";
 import cheerio from "cheerio";
+import { ICardListUrl } from "../shared/interfaces/ICardListUrl";
 
 /**
  * This file looks for the navigation section
  * of the DBS-CardGame website and scrapes all the links
  * to the pages that showcase the cards.
  */
-const AxiosInstance = axios.create();
 
+/**
+ * Constants
+ */
+const AxiosInstance = axios.create();
 const DBS_DATA_BASE_URL =
     "http://www.dbs-cardgame.com/us-en/cardlist/?search=true&category=";
 const HREF_REGEXP = /.*?\?.*?category=(?<category>.+)$/;
 
-
+/**
+ * Exported function that will obtain the nav list and returns an array
+ * @param cardListUrl - Url that points to the card list page of dbs-cardgame.com
+ * @returns @type ICardListUrl[] 
+ */
 export async function GetCardListUrls(cardListUrl: string) {
     const response = await AxiosInstance.get(cardListUrl);
     const body = await response.data;
@@ -20,12 +28,17 @@ export async function GetCardListUrls(cardListUrl: string) {
 
     const navListItems = $("div#snaviList li a");
     console.log(navListItems);
-    const urlsInfo = navListItems.toArray().map(extractInfoFromItemNode($));
+    const urlsInfo: ICardListUrl[] = navListItems.toArray().map(extractInfoFromItemNode($)) as ICardListUrl[];
 
     return urlsInfo;
 }
 
-function extractInfoFromItemNode($) {
+/**
+ * Extracts information from a Node on the DOM
+ * @param $ - Dom that we are extracting information from
+ * @returns a ICardListUrl from each Item Node
+ */
+function extractInfoFromItemNode($){
     return (itemNode) => {
         const cheerioElement = $(itemNode);
         const href = cheerioElement.attr("href");
@@ -36,7 +49,7 @@ function extractInfoFromItemNode($) {
             href,
             category,
             url: `${DBS_DATA_BASE_URL}${category}`,
-        };
+        } as ICardListUrl;
     };
 }
 
